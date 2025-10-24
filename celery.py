bc9850ee8d71a2a05e925782908a680373114d3b
Haymanot Asmare <hasmare463@gmail.com>
2025-10-24 01:46:01 -0700

    create apps

---
+++ celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CFIT.settings')
+app = Celery('CFIT')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
