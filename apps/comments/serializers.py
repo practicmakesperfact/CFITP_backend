f8b0284b608d5345c7a9aa00d404011e46e06767
Haymanot Asmare <hasmare463@gmail.com>
2025-10-26 21:36:53 -0700

    Add CommentSerializer with nested author and read-only fields

--- apps/comments/serializers.py
+++ apps/comments/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from .models import Comment
 from apps.users.serializers import UserSerializer
 
-class CommentSeerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
 
     class Meta:
         model = Comment
         fields ='__all__'
         read_only_fields = ['id','created_at','update_at']
+
