9c8372de50e476df5e5863af18897d16734e51cc
Haymanot Asmare <hasmare463@gmail.com>
2025-10-26 22:02:35 -0700

    Add CommentService with create, update, delete logic and mention notifications

---
+++ apps/comments/services.py
@@ -0,0 +1,44 @@
+from .models import Comment
+from apps.notifications.services import NotificationService
+import re
+
+class CommentService:
+    @staticmethod
+    def create_comment(user,issue,data):
+        Comment=Comment.objects.create(
+            author=user,
+            issue=issue,**data)
+        #cheeck mentions @username
+        mentions=re.findall(r'@(\w+)',data['content'])
+        for username in mentions:
+            try:
+                mentioned_user=user.objects.get(email_icontains=username)
+                NotificationService.create_notification(
+                    recipient=mentioned_user,
+                    message=f'You were mentioned in a comment on issue {issue.title}',
+                    type='mention',
+                    issue=issue,
+
+                )
+            except user.DoesNotExist:
+                pass
+        NotificationService.create_notification(
+            recipient=issue.assignee,
+            message=f'New comment on issue {issue.title}',
+            type='comment',
+            issue=issue,
+        )
+        return Comment
+    @staticmethod
+    def update_comment(comment,data,user):
+        if comment.author !=user:
+            raise PermissionDenied('You can only edit your own contents')
+        comment.content=data.get('content',comment.content)
+        comment.save()
+        return comment  
+    @staticmethod
+    def delete_comment(comment,user):
+        if  comment.author !=user:
+            raise PermissionDenied('You can only delete your own contents')
+        comment.delete()
+
