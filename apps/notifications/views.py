0f1093e959834acf06e8c0605c65ea95e9c12362
Haymanot Asmare <hasmare463@gmail.com>
2025-10-28 03:01:13 -0700

    Add NotificationViewSet with user filtering and mark-read endpoint

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import NotificationSerializer
+from .models import Notification
+from .services import NotificationService
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user).order_by('-created_at')
+
+    @action(detail=True, methods=['post'], url_path='mark-read')
+    def mark_read(self, request, pk=None):
+        notification = self.get_object()
+        NotificationService.mark_as_read(notification)
+        return Response(NotificationSerializer(notification).data)
