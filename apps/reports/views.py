4cda6066c444ed4630a35bd114ee310ac056ec02
Haymanot Asmare <hasmare463@gmail.com>
2025-10-29 22:57:05 -0700

    add ReportViewSet for handling report creation and user-specific queries

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .serializers import ReportSerializer
+from .models import Report
+from .services import ReportService
+from rest_framework.permissions import IsAuthenticated
+from apps.users.permissions import IsStaffOrManager
 
-# Create your views here.
+class ReportViewSet(viewsets.ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrManager]
+
+    def perform_create(self, serializer):
+        report_type = self.request.data.get('type')
+        report = ReportService.request_report(self.request.user, report_type)
+        serializer.instance = report
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
